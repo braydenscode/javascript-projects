97a54aba7b0651861ec9fa64172babad89d8d7ef
Owner <braydens.coding@gmail.com>
2024-07-22 23:36:25 -0500

    Pushing leftover edited files

--- functions/try-it/reverse.js
+++ functions/try-it/reverse.js
@@ -1,5 +1,6 @@
 function reverse(str) {
-   let lettersArray = str.split('');
-   let reversedLettersArray = lettersArray.reverse();
-   return reversedLettersArray.join('');
+   let lettersArray = str.split('').reverse();
+   return lettersArray.join('');
 }
+
+console.log(reverse('reverse'));
