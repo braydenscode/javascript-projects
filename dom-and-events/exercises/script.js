5c6ce4b721a06930104752b2cc7e5eb346a9edba
Owner <braydens.coding@gmail.com>
2024-08-01 19:02:05 -0500

    CH:23 DOM & Events Exercise [COMPLETED]

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,32 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
+    function liftoff() {
+        paragraph.innerHTML = "Houston, we have liftoff!"
+        console.log("Liftoff achieved.");
+    };
+
+    button.addEventListener("click", liftoff);
+
+    missionAbort.addEventListener("mouseover", event => {
+        let element = event.target;
+        element.style.backgroundColor = "red";
+        console.log("Hovering abort!");
+    });
+
+    missionAbort.addEventListener("mouseout", event => {
+        let element = event.target;
+        element.style.backgroundColor = "";
+        console.log("Abort avoided.");
+    });
+
+    missionAbort.addEventListener("click", function() {
+    let boolean = confirm("Are you sure you want to abort the mission?");
+     if (boolean === true) {
+        paragraph.innerHTML = "Mission aborted! Space shuttle returning home."
+     }
+    });
     
 }
 
-window.addEventListener("load", init);
+window.addEventListener("load", init)
