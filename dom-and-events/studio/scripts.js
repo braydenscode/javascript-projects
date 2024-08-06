76f98d64f132b672a24f3bb2a2cece3bdb41a029
Owner <braydens.coding@gmail.com>
2024-08-06 16:46:27 -0500

    CH:23 Studio [COMPLETE]

--- dom-and-events/studio/scripts.js
+++ dom-and-events/studio/scripts.js
@@ -1,6 +1,9 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
 window.addEventListener("load", function(event) {
+
+    let altitude = 0;
+
     let takeoff = document.getElementById("takeoff");
     takeoff.addEventListener("click", function(event) {
       let confirmation = window.confirm("Confirm that the shuttle is ready for takeoff");
@@ -9,46 +12,64 @@ window.addEventListener("load", function(event) {
             let spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
             document.getElementById("shuttleBackground").style.backgroundColor = "blue";
             flightStatus.innerHTML = "Shuttle in flight.";
-            spaceShuttleHeight.innerHTML = "10000";
-            console.log("Confirms");
+            altitude = 10000;
+            spaceShuttleHeight.innerHTML = altitude;
+            rocketPosY += 10;
+            rocket.style.marginBottom = rocketPosY + 'px';
         }
     })
     let landing = document.getElementById("landing");
     landing.addEventListener("click", function(event) {
         window.alert("The shuttle is landing. Landing gear engaged.")
         flightStatus.innerHTML = "The shuttle has landed.";
-        document.getElementById("shuttleBackground").style.backgroundColor = "";
-        spaceShuttleHeight.innerHTML = "0";
+        resetRocket();
     })
     let abort = document.getElementById("missionAbort");
     abort.addEventListener("click", function (event) {
         let confirm = window.confirm("Confirm that you want to abort the mission.")
         if (confirm === true) {
             flightStatus.innerHTML = "Mission aborted.";
-            document.getElementById("shuttleBackground").style.backgroundColor = "";
-            spaceShuttleHeight.innerHTML = "0";
+            resetRocket();
         }
     })
 
-    let up = document.getElementById("up");
-    let down = document.getElementById("down");
-    let right = document.getElementById("right");
-    let left = document.getElementById("left");
     let rocket = document.getElementById("rocket");
-    let shuttleBackground = document.getElementById("shuttleBackground");
-    let currentPosition = 0;
-    let position = rocket.style.position = "relative";
-    up.addEventListener("click", function (event) {
+    let rocketPosX = 0;
+    let rocketPosY = 0;
 
-        position.style.top = "10px";
-    })
-    down.addEventListener("click", function (event) {
-        rocket.style.marginBottom = "10px";
-    })
-    right.addEventListener("click", function (event) {
-        rocket.style.marginTop = 10 + "px";
-    })
-    left.addEventListener("click", function (event) {
+    document.addEventListener("click", function (event) {
+        let bkgWidth = parseInt(window.getComputedStyle(shuttleBackground).getPropertyValue('width'));
 
-    })
-})
+        if (event.target.id === "up" && altitude < 250000) {
+            rocketPosY += 10;
+            rocket.style.marginBottom = rocketPosY + 'px';
+            altitude += 10000;
+            spaceShuttleHeight.innerHTML = altitude;
+        }
+        if (event.target.id === "down" && altitude > 0) {
+            rocketPosY -= 10;
+            rocket.style.marginBottom = rocketPosY + 'px';
+            altitude -= 10000;
+            spaceShuttleHeight.innerHTML = altitude;
+        }
+        if (event.target.id === "right"&& rocketPosX < (bkgWidth / 2 - 40)) {
+            rocketPosX += 10;
+            rocket.style.marginLeft = rocketPosX + 'px';
+        }
+        if (event.target.id === "left" && rocketPosX > -(bkgWidth / 2 - 40)) {
+            rocketPosX -= 10;
+            rocket.style.marginLeft = rocketPosX + 'px';
+        }
+    });
+
+    function resetRocket() {
+        document.getElementById("shuttleBackground").style.backgroundColor = "";
+        altitude = 0;
+        spaceShuttleHeight.innerHTML = altitude;
+        rocketPosX = 0;
+        rocketPosY = 0;
+        rocket.style.marginLeft = rocketPosX + 'px';
+        rocket.style.marginBottom = rocketPosY + 'px';
+    }
+
+});
